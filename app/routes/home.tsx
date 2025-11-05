686c26a2ad5ee9cdcfa9eee2c95ba744ad9dd01c
GlobalGurke <107066410+GlobalGurke@users.noreply.github.com>
2025-11-05 22:01:09 +0100

    PrivacyUpdate

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -17,10 +17,8 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   const infoItems = [
-  { icon: <FaRegCalendarAlt />, text: <p className="cursor-default">03.01.2005, Baden Baden</p>, title: "Geburtsdatum" },
-  { icon: <FaMapMarker />, text: <p  className="cursor-default">Gernsbach</p>, title: "Wohnort" },
   { icon: <FaEnvelope />, text: <p  className="cursor-default">k.krafti@web.de</p>, title: "E-Mail" },
-  { icon: <FaLandmark />, text: <p  className="cursor-default">Karlsruher Insititut für Technologie</p>, title: "Aktuell Student beim:" },
+  { icon: <FaLandmark />, text: <p  className="cursor-default">Wirtschaftsingenieur</p>, title: "Studiengang am Karlsruher Institut für Technologie" },
   { icon: <FaLinkedin />, text: <a className="hover:text-blue-500 transition-colors delay-20" href="https://www.linkedin.com/in/kevin-kraft-b86206395/" target="_blank" rel="noopener noreferrer">Kevin Kraft</a>, title: "LinkedIn"},
   { icon: <FaGithub />, text: <a className="hover:text-blue-500 transition-colors delay-20" href="https://github.com/GlobalGurke" target="_blank" rel="noopener noreferrer">Github</a> , title: "Projekte auf Github"},
 ];
@@ -53,9 +51,13 @@ export default function Home() {
             <div className="ml-8 flex-1">
               <div className="w-full bg-gray-800/70 backdrop-blur-md shadow-xl rounded-xl p-6  mx-4 ">
                 <h1 className="border-b-2 border-blue-500"><strong>Über Mich</strong></h1>
-                <p className="my-4">Mein Name ist Kevin Kraft. Ich habe mein Abitur in Baden-Baden abgeschlossen und studiere derzeit Wirtschaftsingenieurwesen (Bachelor) am Karlsruher Institut für Technologie. Aktuell befinde ich mich im 5. Semester und plane, mein Studium im Sommersemester 2027 abzuschließen.
-                Neben meinem Studium interessiere ich mich leidenschaftlich für Softwareentwicklung und Projektmanagement. In diesem Bereich habe ich bereits mehrere private Projekte umgesetzt, darunter die Entwicklung von Spieleservern sowie Plugins für verschiedene Plattformen. Weitere Informationen zu meinen Projekten finden Sie auf meiner Projektseite. 
-                </p>
+                <p className="my-4">Hi, ich bin Kevin Kraft. Ich studiere Wirtschaftsingenieurwesen am Karlsruher Institut für Technologie.
+Neben dem Studium entwickle ich Software und manage Projekte. Meine Erfahrungen umfassen unter anderem:</p>
+                <p>- Entwicklung von Spieleservern und Plugins für verschiedene Plattformen</p>
+                <p>- Anwendung von Projektmanagement-Methoden</p>
+                <br></br>
+                <p>Weitere Projekte findest du auf meiner Projektseite.</p>
+
               </div>
               <div className="w-full bg-gray-800/70 backdrop-blur-md shadow-xl rounded-xl p-6 mx-4  my-4">
 
