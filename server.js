5341cea1fffee89399f5d9c5a343eb6e405ac86f
GlobalGurke <107066410+GlobalGurke@users.noreply.github.com>
2025-11-05 16:18:19 +0100

    Fix2

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { createRequestHandler } from "@react-router/express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const buildPath = path.join(__dirname,"build", "build", "server");
+app.use(express.static(path.join(__dirname, "build", "client")));
+
+app.all(
+  "*",
+  createRequestHandler({
+    build: await import(`${buildPath}/index.js`),
+  }),
+);
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
